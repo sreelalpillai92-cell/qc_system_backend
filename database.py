f3ccfa22d48d7df7041aa0c72ee2426e0115226c
sreelalpillai92-cell <sreelalpillai92@gmail.com>
2026-01-01 01:13:58 +0400

    Switch to SQLite DATABASE_URL for Render

--- database.py
+++ database.py
@@ -1,13 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:qwerty1234@localhost:5432/qaqc_db"
+DATABASE_URL = "sqlite:///./database.db"
 
-
-
-
-
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
